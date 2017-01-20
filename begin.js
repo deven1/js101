e5716fd1e719fbf98fdaba60ceffc71d9d8f3f30
deven1 <deven@sloan.mit.edu>
2017-01-20 10:49:36 -0500

    Created several starter examples

---
+++ begin.js
@@ -0,0 +1,32 @@
+// Comment
+console.log("Jay Swaminarayan".length);
+
+//confirm ("Click Ok to Continue");
+
+//prompt ('Where are you heading');
+
+string = "Deven";
+console.log(string.length);
+
+if(string.length > 3) {console.log("Nice")}
+
+console.log(15>4); // "True" is printed
+
+console.log(8*2 == 15); // "False" is printed"	
+
+if(100 % 2 == 0) {console.log("Even Number")}
+	else {console.log("Odd Number")}
+
+console.log("Impossible".substring(2, 10));
+
+var myName = "Deven Bhatt"
+console.log (myName);
+
+myName = myName.substring(0,5);
+console.log(myName);
+
+//var yourName = prompt ("What is your name?");	
+var welcomeMsg = "Welcome";
+console.log (welcomeMsg.concat(' ', myName));
+
+
